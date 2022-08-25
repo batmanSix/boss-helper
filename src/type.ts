8980ab87d6a9aeb3962473521b895868cdcf53fc
kangbo <841331654@qq.com>
2022-08-25 11:33:07 +0800

    feat:新增转大写字母方法

--- src/type.ts
+++ src/type.ts
@@ -10,3 +10,16 @@
  * @beta
  */
  export const hello = (str: string) => `hello ${str}`
+
+ /**
+ * 返回 helloUpperCase 开头的字符串
+ * @param str - input string
+ * @returns 'HELLO xxx' 的大写
+ * @example
+ * ```ts
+ * hello('ts') => 'HELLO TS'
+ * ```
+ *
+ * @beta
+ */
+export const helloUpperCase = (str: string) => `hello ${str}`.toLocaleUpperCase()
